d3a3271e0d9b7faf2693ec9735c01a7915f070a7
mifthulyn07 <mifthulyn07@gmail.com>
2024-05-24 21:59:34 +0700

    add skill vue

---
+++ Javascript/5.vue.js/10.toDoListApp/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// add this
+import "./index.css";
+
+createApp(App).mount("#app");
---
+++ Javascript/5.vue.js/10.toDoListApp/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
